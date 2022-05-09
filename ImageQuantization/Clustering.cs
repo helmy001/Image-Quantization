f46479c3f8c5a1297e9e4003d6b9c975e5e544f9
khaled <khaled20191700224@cis.asu.edu.eg>
2022-05-10 00:43:48 +0200

    Distinct

--- ImageQuantization/Clustering.cs
+++ ImageQuantization/Clustering.cs
@@ -7,10 +7,36 @@ namespace ImageQuantization
 {
     internal class Clustering
     {
-        public void Prims_algo()
+        public static List<RGBPixel> Detailed_Color;
+
+        public static void Properties_Colors(RGBPixel[,] Colored_Image)
         {
+            bool[,,] Appeared_Color = new bool[256, 256, 256];
 
+            Detailed_Color = new List<RGBPixel>();
 
+            int Image_Height = ImageOperations.GetHeight(Colored_Image);
+            int Image_Width = ImageOperations.GetWidth(Colored_Image);
+            int Height_Count = 0;
+            int Width_Count = 0;
+
+            while (Height_Count < Image_Height)
+            {
+                while (Width_Count < Image_Width)
+                {
+                    byte Red_Colored = Colored_Image[Height_Count, Width_Count].red;
+                    byte Green_Colored = Colored_Image[Height_Count, Width_Count].green;
+                    byte Blue_Colored = Colored_Image[Height_Count, Width_Count].blue;
+                    if (Appeared_Color[Red_Colored, Green_Colored, Blue_Colored] == false)
+                    {
+                        Appeared_Color[Red_Colored, Green_Colored, Blue_Colored] = true;
+                        Detailed_Color.Add(Colored_Image[Height_Count, Width_Count]);
+                    }
+                    Width_Count++;
+                }
+                Width_Count = 0;
+                Height_Count++;
+            }
         }
     }
 }
